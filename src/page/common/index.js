c496e012376b4c761363b9a63f7832d7a39fb560
lei <893052525@qq.com>
2019-01-09 17:53:10 +0800

    footer

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,9 +2,10 @@
 * @Author: ManMan
 * @Date:   2019-01-05 21:40:03
 * @Last Modified by:   lei
-* @Last Modified time: 2019-01-09 14:12:10
+* @Last Modified time: 2019-01-09 17:49:28
 */
 
 
 require('./layout.css');
 require('node_modules/font-awesome/css/font-awesome.min.css');
+require('./footer/index.css');
