be5d3b7736e2e3ace4d05d940777ee32e891ad86
banana <893052525@qq.com>
2019-05-10 13:50:03 +0800

    about

---
+++ src/page/about/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: banana
+* @Date:   2019-05-10 13:03:20
+* @Last Modified by:   banana
+* @Last Modified time: 2019-05-10 13:03:39
+*/
+require('./index.css');
+require('page/common/header/index.js');
